495428950deec0105faee56010b19948a1203a8c
Burrel Vann Jr <burrelvannjr@dhcp-v223-177.vp.reshsg.uci.edu>
2016-03-10 10:59:40 -0800

    cleaned up disp scraper

--- disp_scraper.py
+++ disp_scraper.py
@@ -7,49 +7,51 @@ os.chdir('/Users/burrelvannjr/Desktop/')
 
 ids=[]
 names=[]
+pages=[]
 urls=[]
 details=[]
+itemlist=[]
 
 site = "https://weedmaps.com/dispensaries/in/united-states"
 text = requests.get(site).text
 soup = BeautifulSoup(text)
 soup = soup.encode("utf-8")
-id = re.findall(',"wmid":(.*?),"url',soup)
-ids.append(id)
-names = re.findall('","name":"(.*?)","avatar_url"',soup)
-names.append(names)
-names.pop(300)
-pages = re.findall(',"url":"http(.*?)","name":"',soup)
-for i in pages:
-	i = "http" + i
-	detail = i + "#/details"
-	urls.append(i)
+
+items = re.findall(',"wmid":(.*?),"longitude"',soup)
+
+for i in items:
+	i = "wmid:" + i
+	i = i.encode("utf-8")
+	itemlist.append(i)
+
+for i in itemlist:
+	id = re.findall('wmid:(.*?),"url',i)
+	ids.append(id)
+	name = re.findall('","name":"(.*?)","avatar_url"',i)
+	names.append(name)
+	site = re.findall(',"url":"http(.*?)","name":"',i)
+	pages.append(site)
+
+ids = map(','.join, ids)
+names = map(','.join, names)
+pages = map(','.join, pages)
+
+for page in pages:
+	page = "http" + page
+	urls.append(page)
+	detail = page + "#/details"
 	details.append(detail)
 
 
 
-
-
-
-
-
-names = re.findall('',soup)
-
-names.append(name)
-time.append(year)
-fullnames.append(name)
-fulltime.append(year)
-
-
-#converts list of lists into a list, concatenating by everything that falls between major , (such as [], [] as two items not minor commas within elements)
-names = map(','.join, names)
-
-cities = zip(names,time)
-
-csvfile = "cities" + year + ".csv"
+disps = zip(ids,names)
+	
+csvfile = "disp.csv"
 
 with open(csvfile, "w") as output:
 	writer = csv.writer(output, lineterminator='\n')
 	#writer.writerow(["Dispensary Name", "Address", "Numbers", "Website"])
-	writer.writerow(["v1", "Year"])
-	writer.writerows(cities)
+	writer.writerow(["id", "names"])
+	writer.writerows(disps)
+
+
