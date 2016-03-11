82f84dd42e379e52c91a8d8fc6341b9be8d9e4fd
Burrel Vann Jr <burrelvannjr@dhcp-v223-177.vp.reshsg.uci.edu>
2016-03-10 21:30:07 -0800

    each detail page scraped, added phones, cities, zips, states, and licenses

--- disp_scraper.py
+++ disp_scraper.py
@@ -11,6 +11,13 @@ pages=[]
 urls=[]
 details=[]
 itemlist=[]
+licenses=[]
+addresses=[]
+cities=[]
+states=[]
+zips=[]
+phones=[]
+itemlist2=[]
 
 site = "https://weedmaps.com/dispensaries/in/united-states"
 text = requests.get(site).text
@@ -31,16 +38,53 @@ for i in itemlist:
 	names.append(name)
 	site = re.findall(',"url":"http(.*?)","name":"',i)
 	pages.append(site)
+	license = re.findall(',"license_type":"(.*?)","address":"',i)
+	licenses.append(license)
+	address = re.findall(',"address":"(.*?)","city":"',i)
+	addresses.append(address)
+	city = re.findall(',"city":"(.*?)","state":"',i)
+	cities.append(city)
+	state = re.findall(',"state":"(.*?)","zip_code":"',i)
+	states.append(state)
+	zipc = re.findall(',"zip_code":"(.*?)","phone_number":"',i)
+	zips.append(zipc)
+	phone = re.findall(',"phone_number":"(.*?)","latitude"',i)
+	phones.append(phone)
+
 
 ids = map(','.join, ids)
 names = map(','.join, names)
 pages = map(','.join, pages)
+licenses = map(','.join, licenses)
+addresses = map(','.join, addresses)
+cities = map(','.join, cities)
+states = map(','.join, states)
+zips = map(','.join, zips)
+phones = map(','.join, phones)
+phones = phones.replace("(","")
+
+addresses = [a.replace('CALL FOR VERIFICATION', 'N/A') for a in addresses]
+cities = [c.replace('CALL FOR VERIFICATION', 'N/A') for c in cities]
+zips = [z.replace('  OPEN 24/7', '') for z in zips]
+zips = [z.replace('CALL FOR VERIFICATION', 'N/A') for z in zips]
+phones = [p.replace('(', '') for p in phones]
+phones = [p.replace(')', '') for p in phones]
+phones = [p.replace('-', '') for p in phones]
+phones = [p.replace(' ', '') for p in phones]
 
 for page in pages:
 	page = "http" + page
 	urls.append(page)
 	detail = page + "#/details"
 	details.append(detail)
+
+for det in details:
+	newpage = det
+	text2 = requests.get(newpage).text
+	soup2 = BeautifulSoup(text2)
+	soup2 = soup.encode("utf-8")
+	itemlist2.append(soup2)
+
 
 
 
